ddd7b6fc200ff6d39f3b7c5f119c1be50a00576c
ZelihaOrhann <zeliha.social1@gmail.com>
2024-10-05 18:44:35 +0300

    I completed the UI side of the project

---
+++ MyPortfolio/ViewComponents/_PortfolioComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _PortfolioComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyPortfolio/ViewComponents/_StatisticComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _StatisticComponentPartial : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+
+    }
+}
